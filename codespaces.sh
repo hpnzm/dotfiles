7e7618afbc533185ded7bff6a98b0245ff22e470
Phuong Nam <47148464+hpnzm@users.noreply.github.com>
2024-03-22 20:27:57 +0700

    Update codespaces.sh

--- codespaces.sh
+++ codespaces.sh
@@ -5,12 +5,14 @@ git config --global user.name "hpnzm"
 git config --global user.email "theweedsmokers69@gmail.com"
 
 # create SSH key
-mkdir ../ssh
-ssh-keygen -t ed25519 -C "theweedsmokers69@gmail.com" -f ../ssh/sshkey -N ""
-chmod 400 ../ssh/sshkey
-echo -e "\nHere's your SSH key 😆:"
-cat ../ssh/sshkey.pub
-echo "Add your SSH key to GitHub: https://github.com/settings/ssh/new"
+if [ ! -d ../ssh ]; then
+  mkdir ../ssh
+  ssh-keygen -t ed25519 -C "theweedsmokers69@gmail.com" -f ../ssh/sshkey -N ""
+  chmod 400 ../ssh/sshkey
+  echo -e "\nHere's your SSH key 😆:"
+  cat ../ssh/sshkey.pub
+  echo "Add your SSH key to GitHub: https://github.com/settings/ssh/new"
+fi
 
 # functions
 add_ssh () {
